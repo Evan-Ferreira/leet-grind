b40d953b0febc2f8f5b7a2432004448e52879bc4
Evan Ferreira <132397646+Evan-Ferreira@users.noreply.github.com>
2026-01-25 12:34:24 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -5,9 +5,9 @@ class Solution:
             if x + y == 0:
                 # base case: (0, 0)
                 return 0
-            elif x + y == 2:
-                # base case: (1, 1), (0, 2), (2, 0)
-                return 2
+            # elif x + y == 2:
+            #     # base case: (1, 1), (0, 2), (2, 0)
+            #     return 2
             else:
                 return min(dfs(abs(x - 1), abs(y - 2)), dfs(abs(x - 2), abs(y - 1))) + 1
 
