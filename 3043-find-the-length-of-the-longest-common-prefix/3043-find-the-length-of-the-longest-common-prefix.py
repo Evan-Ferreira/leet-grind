879f91da685333ea605cf003862e4044b56e58d0
Evan Ferreira <132397646+Evan-Ferreira@users.noreply.github.com>
2026-02-05 22:52:45 -0500

    Time: 129 ms (98.47%), Space: 29.8 MB (75.35%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.py
@@ -1,43 +1,20 @@
-class Node:
-    def __init__(self):
-        self.children = defaultdict(Node)
-
-class Trie:
-    def __init__(self):
-        self.root = Node()
-    
-    def insert(self, word):
-        curr = self.root
-        for char in word:
-            if char not in curr.children:
-                curr.children[char] = Node()
-            curr = curr.children[char]
-    
-    def getCount(self, word):
-        curr = self.root
-        count = 0
-        for char in word:
-            if char not in curr.children:
-                break
-            curr = curr.children[char]
-            count += 1  
-        return count
-
 class Solution:
     def longestCommonPrefix(self, arr1: List[int], arr2: List[int]) -> int:
-        arr1 = [str(i) for i in arr1]
-        arr2 = [str(i) for i in arr2]
-
-        res = 0
-        trie = Trie()
-
-        for s in arr2:
-            trie.insert(s)
+        if len(arr1) > len(arr2):
+            arr1, arr2 = arr2, arr1
         
-        for s in arr1:
-            res = max(res, trie.getCount(s))
+        s = set()
+        for n in arr1:
+            while n and n not in s:
+                s.add(n)
+                n = n // 10
+        
+        res = 0
+        for n in arr2:
+            while n and n not in s:
+                n = n // 10
+            
+            if n:
+                res = max(res, len(str(n)))
         
         return res
-        
-
-        
