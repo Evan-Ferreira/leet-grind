707b407db8432c83dcd8caac688adb980af579e0
Evan Ferreira <132397646+Evan-Ferreira@users.noreply.github.com>
2026-02-12 13:48:02 -0500

    Time: 145 ms (47.64%), Space: 32.4 MB (56%) - LeetHub

--- 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
+++ 2369-check-if-there-is-a-valid-partition-for-the-array/2369-check-if-there-is-a-valid-partition-for-the-array.py
@@ -1,34 +1,20 @@
 class Solution:
     def validPartition(self, nums: List[int]) -> bool:
-        m = defaultdict(bool)
-        def dfs(i):
-            if i >= len(nums):
-                m[i] = True
-                return True
-            if i in m:
-                return m[i]
-            if (i + 1) < len(nums) and nums[i] == nums[i + 1]:
-                res = dfs(i + 2)
-                if res:
-                    m[i] = True
-                    return True
-                else: 
-                    m[i] = False
-            if (i + 2) < len(nums) and nums[i] == nums[i + 1] == nums[i + 2]:
-                res = dfs(i + 3)
-                if res:
-                    m[i] = True
-                    return True
-                else: 
-                    m[i] = False
-            if (i + 2) < len(nums) and (nums[i] + 2) == (nums[i + 1] + 1) == (nums[i + 2]):
-                res = dfs(i + 3)
-                if res:
-                    m[i] = True
-                    return True
-                else: 
-                    m[i] = False
-            return m[i]
-        return dfs(0)
+        two = nums[-1] == nums[-2]
+        if len(nums) == 2:
+            return two
+        
+        three = (nums[-1] == nums[-2] == nums[-3]) or (nums[-3] + 1 == nums[-2] == nums[-1] -1)
+
+        dp = [three, two, False]
+        for i in range(len(nums) - 4, -1, -1):
+            curr = (nums[i] == nums[i + 1]) and dp[1]
+            curr = curr or (
+                (nums[i] == nums[i + 1] == nums[i + 2] or
+                nums[i] + 1 == nums[i + 1] == nums[i + 2] - 1) and 
+                dp[2]
+            )
+            dp = [curr, dp[0], dp[1]]
+        return dp[0]
             
 
