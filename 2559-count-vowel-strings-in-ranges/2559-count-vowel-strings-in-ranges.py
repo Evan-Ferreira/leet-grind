4d0d9dae497110141b89e3b8446779745d18f60f
Evan Ferreira <132397646+Evan-Ferreira@users.noreply.github.com>
2026-02-21 13:39:30 -0500

    Time: 23 ms (39.44%), Space: 48.9 MB (19.29%) - LeetHub

--- 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.py
+++ 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.py
@@ -6,7 +6,7 @@ class Solution:
         
         for i in range(len(words)):
             prefix[i + 1] = prefix[i] + (1 if words[i][0] in vowels and words[i][-1] in vowels else 0)
-        print(prefix)
+
         for index in range(len(queries)):
             l, r = queries[index][0], queries[index][1]
             res[index] = prefix[r + 1] - prefix[l]
