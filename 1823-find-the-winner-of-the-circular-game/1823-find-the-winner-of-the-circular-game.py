3d05cadeb214754130cf6bf92feec033c47f9468
Evan Ferreira <132397646+Evan-Ferreira@users.noreply.github.com>
2026-02-24 13:52:41 -0500

    Time: 0 ms (100%), Space: 19.3 MB (52.29%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,35 +1,7 @@
-class Node:
-    def __init__(self, val, next=None, prev=None):
-        self.val = val
-        self.prev = prev
-        self.next = next
-
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-
-        def removeNode(node):
-            prev, nxt = node.prev, node.next
-            prev.next = nxt
-            nxt.prev = prev
-
-        root = Node(1)
-        node = root
-        for num in range(2, n + 1):
-            newNode = Node(num)
-            node.next = newNode
-            newNode.prev = node
-            node = newNode
-        node.next = root
-        root.prev = node
-
-        curr = root
-        while curr.next != curr:
-            for _ in range(k - 1):
-                curr = curr.next
-            nxt = curr.next
-            removeNode(curr)
-            curr = nxt
-
-        return curr.val
-
-            
+        ans = 0
+        for i in range(2, n + 1):
+            ans = (ans + k) % i
+        # add 1 to convert back to 1 indexing
+        return ans + 1
