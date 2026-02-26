0614a34e3962026b3161f379b2b8456d4b2935fa
Evan Ferreira <132397646+Evan-Ferreira@users.noreply.github.com>
2026-02-25 22:13:01 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,8 +1,5 @@
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
-        """
-        Do not return anything, modify nums in-place instead.
-        """
         for i in range(len(nums) - 1, 0, -1):
             if nums[i - 1] < nums[i]:
                 minIndex = i
@@ -10,6 +7,6 @@ class Solution:
                     if nums[j] < nums[minIndex] and nums[j] > nums[i - 1]:
                         minIndex = j
                 nums[i - 1], nums[minIndex] = nums[minIndex], nums[i - 1]
-                nums[i:] = sorted(nums[i:])
+                nums[i:] = nums[i:][::-1]
                 return
-        nums.sort()
+        nums.reverse()
