32e851f9ed7761a0443c9437fcb13c5f955e5dbc
Evan Ferreira <132397646+Evan-Ferreira@users.noreply.github.com>
2026-03-02 17:57:51 -0500

    Time: 0 ms (100%), Space: 19.2 MB (85.06%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -5,9 +5,8 @@ class Solution:
 
         tI = 0
         for sI in range(len(s)):
-            if tI < len(t) and s[sI] != t[tI]:
-                while tI < len(t) and s[sI] != t[tI]:
-                    tI += 1
+            while tI < len(t) and s[sI] != t[tI]:
+                tI += 1
                         
             if tI >= len(t):
                 return False
