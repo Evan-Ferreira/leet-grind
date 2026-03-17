545ea46e6e850347982159ef06538886185a731d
Evan Ferreira <132397646+Evan-Ferreira@users.noreply.github.com>
2026-03-17 09:50:22 -0400

    Time: 359 ms (5.92%), Space: 26.8 MB (5.01%) - LeetHub

--- 1980-find-unique-binary-string/1980-find-unique-binary-string.py
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.py
@@ -2,22 +2,27 @@ class Solution:
     def findDifferentBinaryString(self, nums: List[str]) -> str:
         n = len(nums[0])
         nums = set(nums)
+        res = [""]
+
+        for i in range(n):
+            seen = set()
+            tmpRes = []
+            for num in nums:
+                seen.add(num[i])
+            for j in range(len(res)):
+                if len(seen) == 2:
+                    tmpRes.append(res[j] + "1")
+                    tmpRes.append(res[j] + "0")
+                elif "1" in seen:
+                    return res[-1] + ("0" * (n - i))
+                else:
+                    return res[-1] + ("1" * (n - i))
+            res = tmpRes
+        for n in res:
+            if n not in nums:
+                return n
 
 
-        def dfs(curr):
-            if len(curr) == n and curr not in nums:
-                return curr
-            
-            if len(curr) == n:
-                return ""
-            
-            ans1 = dfs(curr + "1")
-            ans0 = dfs(curr + "0")
-            if ans1:
-                return ans1
-            return ans0
-        
-        return dfs("")
             
             
             
