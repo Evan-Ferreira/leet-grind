13751325fbbafceb5b31204a326accb5f6d3079d
Evan Ferreira <132397646+Evan-Ferreira@users.noreply.github.com>
2026-03-22 05:44:34 -0400

    Time: 35 ms (26.64%), Space: 26 MB (90.91%) - LeetHub

--- 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
+++ 1985-find-the-kth-largest-integer-in-the-array/1985-find-the-kth-largest-integer-in-the-array.py
@@ -1,3 +1,11 @@
 class Solution:
     def kthLargestNumber(self, nums: List[str], k: int) -> str:
-        return sorted(nums, key=lambda x:int(x))[len(nums) - k]
+        h = []
+        for n in nums:
+            heapq.heappush_max(h, int(n))
+        
+        res = 0
+        for _ in range(k):
+            res = heapq.heappop_max(h)
+
+        return str(res)
