de3fd015ba15ce008c204889c5f2cd8a94050dca
Evan Ferreira <132397646+Evan-Ferreira@users.noreply.github.com>
2026-03-25 17:25:11 -0400

    Time: 19 ms (83.82%), Space: 22.5 MB (22.95%) - LeetHub

--- 0490-the-maze/0490-the-maze.py
+++ 0490-the-maze/0490-the-maze.py
@@ -3,23 +3,25 @@ class Solution:
         ROWS, COLS = len(maze), len(maze[0])
         directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
         visited = set()
-    
+        r, c = start
+
         def dfs(r, c):
             if (r, c) in visited:
                 return False
-
+            
             if [r, c] == destination:
                 return True
-
+            
             visited.add((r, c))
 
             for dr, dc in directions:
                 row, col = r, c
-                while row >= 0 and row < ROWS and col >= 0 and col < COLS and maze[row][col] == 0:
+                while (row >= 0 and col >= 0 and row < ROWS and col < COLS and maze[row][col] == 0):
                     row, col = row + dr, col + dc
-
+                
                 if dfs(row - dr, col - dc):
                     return True
+            
             return False
-        r, c = start
         return dfs(r, c)
+
