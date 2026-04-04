cb53cc2a66f05c5ebdc6496129fa27b839867cc6
Evan Ferreira <132397646+Evan-Ferreira@users.noreply.github.com>
2026-04-04 12:16:35 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.py
+++ 0441-arranging-coins/0441-arranging-coins.py
@@ -1,8 +1,5 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:
-        if n == 1 or n == 0:
-            return n
-
         for i in range(n):
             n -= i + 1
             if n < 0:
