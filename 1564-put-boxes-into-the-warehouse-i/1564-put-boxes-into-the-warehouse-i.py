6bdd58d7ca075d941c2ec0c2e5f9ec6ab81e4537
Evan Ferreira <132397646+Evan-Ferreira@users.noreply.github.com>
2026-04-06 10:22:24 -0400

    Time: 49 ms (91.25%), Space: 38.9 MB (29.66%) - LeetHub

--- 1564-put-boxes-into-the-warehouse-i/1564-put-boxes-into-the-warehouse-i.py
+++ 1564-put-boxes-into-the-warehouse-i/1564-put-boxes-into-the-warehouse-i.py
@@ -3,24 +3,17 @@ class Solution:
         # positive int[]: boxes, warehouses
         # boxes: width = 1, height[i]
         # warehouses: height of room i is warehouse[i], labelled from 0 to n - 1
-        boxes.sort()
-        j = 0
-        res = 0
-        N = len(warehouse)
+        boxes.sort(reverse = True)
+        i = 0
+        count = 0
 
-        warehouse_mins = [0] * N
-        warehouse_mins[0] = warehouse[0]
-
-        for i in range(1, N):
-            if warehouse[i] < warehouse_mins[i - 1]:
-                warehouse_mins[i] = warehouse[i]
-            else:
-                warehouse_mins[i] = warehouse_mins[i - 1]
-        
-        for i in range(N - 1, -1 , -1):
-            if j < len(boxes) and warehouse_mins[i] < boxes[j]:
-                continue
-            if j < len(boxes) and boxes[j] <= warehouse_mins[i]:
-                j += 1
-                res += 1
-        return res
+        for room in warehouse:
+            while i < len(boxes) and boxes[i] > room:
+                i += 1
+            
+            if i == len(boxes):
+                return count
+            
+            count += 1
+            i += 1
+        return count
