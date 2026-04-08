e0d9b15abd4e1f38e5e6b618f99f0369ed04d99d
Evan Ferreira <132397646+Evan-Ferreira@users.noreply.github.com>
2026-04-08 10:32:52 -0400

    Time: 53 ms (18.8%), Space: 19.9 MB (85.24%) - LeetHub

---
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
@@ -0,0 +1,56 @@
+class Solution:
+    def numDistinctIslands(self, grid: List[List[int]]) -> int:
+        normalized_islands = []
+        visited = set()
+        directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+        ROWS, COLS = len(grid), len(grid[0])
+
+        def bfs(row, col):
+            nonlocal visited
+            q = deque()
+            q.append((row, col))
+            local_visited = set()
+            local_visited.add((row, col))
+            minX, minY = row, col
+
+            while q:
+                row, col = q.popleft()
+
+                for dr, dc in directions:
+                    r, c = row + dr, col + dc
+                    if (r == ROWS or c == COLS or r < 0 or c < 0 or (r, c) in local_visited or grid[r][c] == 0):
+                        continue
+
+                    minX = min(r, minX)
+                    minY = min(c, minY)
+                    local_visited.add((r, c))
+                    q.append((r, c))
+
+            visited |= local_visited
+            ans = set()
+            for r, c in local_visited:
+                ans.add((r - minX, c - minY))
+            
+            return ans
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 1 and (r, c) not in visited:
+                    normalized = bfs(r, c)
+                    new = True
+                    for island in normalized_islands:
+                        isCurrIsland = True
+                        if len(island) != len(normalized):
+                            continue
+                        for row, col in island:
+                            if (row, col) not in normalized:
+                                isCurrIsland = False
+                                break
+                        if isCurrIsland:
+                            new = False
+                            break
+                    if new:
+                        normalized_islands.append(normalized)
+        
+        return len(normalized_islands)
+
