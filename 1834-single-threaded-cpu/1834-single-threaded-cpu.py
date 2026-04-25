b63297e1f33f4c7559722b01f92aaee41b7f2556
Evan Ferreira <132397646+Evan-Ferreira@users.noreply.github.com>
2026-04-25 16:31:36 -0400

    Time: 391 ms (29.01%), Space: 73.2 MB (5.77%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -1,22 +1,23 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        new_tasks = defaultdict(list)
         for i, t in enumerate(tasks):
-            t.append(i)
+            new_tasks[t[0]].append([t[1], i])
+        taskOrder = sorted(new_tasks.keys(), reverse=True)
 
-        tasks.sort(key = lambda t: t[0])
-
-        res, heap = [], []
-        i, time = 0, tasks[0][0]
-
-        while heap or i < len(tasks):
-            while i < len(tasks) and time >= tasks[i][0]:
-                heapq.heappush(heap, [tasks[i][1], tasks[i][2]])
-                i += 1
-            
-            if not heap:
-                time = tasks[i][0]
-            else:
-                prc, index = heapq.heappop(heap)
-                time += prc
-                res.append(index)
+        heap = new_tasks[taskOrder[-1]]
+        heapq.heapify(heap)
+        res = []
+        currTime = taskOrder[-1]
+        taskOrder.pop()
+        while heap:
+            time, index = heapq.heappop(heap)
+            res.append(index)
+            currTime = currTime + time
+            if not heap and taskOrder and currTime < taskOrder[-1]:
+                currTime = taskOrder[-1]
+            while taskOrder and currTime >= taskOrder[-1]:
+                for t in new_tasks[taskOrder[-1]]:
+                    heapq.heappush(heap, t)
+                taskOrder.pop()
         return res
