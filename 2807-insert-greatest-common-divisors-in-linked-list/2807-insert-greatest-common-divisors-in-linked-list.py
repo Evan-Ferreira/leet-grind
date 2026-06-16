9190975ee229171226b638b09c01fcdd9ec53b1b
Evan Ferreira <132397646+Evan-Ferreira@users.noreply.github.com>
2026-06-15 21:28:58 -0700

    Time: 14 ms (53.55%), Space: 23 MB (18.02%) - LeetHub

--- 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.py
+++ 2807-insert-greatest-common-divisors-in-linked-list/2807-insert-greatest-common-divisors-in-linked-list.py
@@ -5,47 +5,14 @@
 #         self.next = next
 class Solution:
     def insertGreatestCommonDivisors(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        mx = head.val
-        tmp = head
-        while tmp:
-            mx = max(tmp.val, mx)
-            tmp = tmp.next
-
-        primes = []
-        not_primes = set()
-        for i in range(2, mx + 1):
-            if i not in not_primes:
-                primes.append(i)
-                n = i
-                for j in range(i, mx):
-                    if j * i > mx:
-                        break
-                    not_primes.add(j * i)
-
-        def getGCD(n1, n2):
-            start = 1
-            for p in primes[::-1]:
-                if n1 % p == 0 and n2 % p == 0:
-                    start = p
-                    break
-            if start == 1:
-                return 1
-            new = start
-            for i in range(2, min(n1, n2)):
-                if start * i > min(n1, n2):
-                    break
-                if n1 % (start * i) == 0 and n2 % (start * i) == 0:
-                    new = start * i
-            return new
-
-        prev = head
-        curr = head.next
-        while curr:
-            new = ListNode()
-            new.val = getGCD(prev.val, curr.val)
-            prev.next = new
-            new.next = curr
-            prev = curr
-            curr = curr.next
+        def gcd(a, b):
+            while b > 0:
+                a, b = b, a % b
+            return a
         
+        curr = head
+        while curr.next:
+            n1, n2 = curr.val, curr.next.val
+            curr.next = ListNode(gcd(n1, n2), curr.next)
+            curr = curr.next.next
         return head
